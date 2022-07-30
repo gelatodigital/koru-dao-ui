a6ede2d501e781b24e15cd548611a7db8320b2af
Renato Cardoso <re2005@gmail.com>
2022-07-30 10:35:55 +0200

    Update CountUpTimer.tsx

--- src/components/globals/CountUpTimer.tsx
+++ src/components/globals/CountUpTimer.tsx
@@ -11,7 +11,7 @@ const CountUpTimer = ({ timestamp }: { timestamp: number }) => {
 
                 {days < 1 && hours > 0 ? `${hours} hours ` : null}
 
-                {hours < 1 && minutes > 0 ? `${minutes} minutes ` : null}
+                {days < 1 && hours < 1 && minutes > 0 ? `${minutes} minutes ` : null}
 
                 {hours < 1 && minutes < 1 &&
                   <span>
