550453a5ec8f789f72955076bf5682578afad987
Renato Cardoso <re2005@gmail.com>
2022-09-07 15:18:41 +0200

    chore: cleanup unused contracts

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,16 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/styles/index.scss';
-import { allChains, configureChains, createClient, WagmiConfig, chain } from 'wagmi';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { AppProvider } from './contexts/AppContext';
 import { infuraProvider } from 'wagmi/providers/infura';
 import { publicProvider } from 'wagmi/providers/public';
 
-const infuraId = import.meta.env.VITE_INFURA_PROJECT_ID;
-
 const { chains, provider, webSocketProvider } = configureChains([chain.polygon, chain.polygonMumbai], [
         publicProvider(),
-        infuraProvider({ infuraId }),
+        infuraProvider(),
     ],
 );
 
