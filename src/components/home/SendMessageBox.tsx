b6555d4876b137a111c892354ed38fcc27f50e23
Renato Cardoso <re2005@gmail.com>
2022-09-07 14:27:33 +0200

    chore: clean up unused & fix dynamic provider

--- src/components/home/SendMessageBox.tsx
+++ src/components/home/SendMessageBox.tsx
@@ -2,7 +2,7 @@ import UiIcon from '../globals/UiIcon';
 import { useContext, useState } from 'react';
 import { AppContext } from '../../contexts/AppContext';
 import { GelatoRelaySDK } from '@gelatonetwork/relay-sdk';
-import { useAccount, useNetwork, useProvider, useSigner, useSignTypedData } from 'wagmi';
+import { useAccount, useNetwork, useProvider, useSigner } from 'wagmi';
 import { BytesLike, ethers, Signer } from 'ethers';
 import { supportedChains } from '../../blockchain/constants';
 import { koruContract } from '../../blockchain/contracts/koruContract.factory';
@@ -11,6 +11,8 @@ import uploadToIPFS from '../../utils/ipfs';
 // @ts-ignore
 import CircularProgress from '../../utils/circularProgress';
 import { CountTimer } from '../globals/CountTimer';
+import { Props } from 'react-inlinesvg';
+import { Provider } from '@ethersproject/providers';
 
 export default function SendMessageBox() {
 
@@ -18,7 +20,6 @@ export default function SendMessageBox() {
     const provider = useProvider();
     const { address } = useAccount();
     const { data: signer } = useSigner();
-    const { signTypedDataAsync } = useSignTypedData();
 
     const { lensHandler, publications, setPublications, userPost, nftId }: any = useContext(AppContext);
 
@@ -53,7 +54,7 @@ export default function SendMessageBox() {
         return path;
     };
 
-    const testLensPost = async () => {
+    const makeLensPost = async () => {
         // Not sure if this is the correct target contract address
         const targetAddress = "0x2334Bb5d5A1547970767315dE048e939C94D6E34";
         const { data } = await getRequestData();
@@ -65,35 +66,11 @@ export default function SendMessageBox() {
             user: address,
         };
 
-        const _provider = new ethers.providers.Web3Provider(window.ethereum as any);
-
-        const relayResponse = await GelatoRelaySDK.relayWithSponsoredUserAuthCall(
+        return await GelatoRelaySDK.relayWithSponsoredUserAuthCall(
             relayRequest as any,
-            _provider,
+            signer?.provider as any,
             'KORU_DAO_KEY',
         );
-
-        console.log(relayResponse);
-    };
-
-    const testIncrementContext = async () => {
-        const counterAddress = "0x30d97B13e29B0cd42e6ebd48dbD9063465bF1997";
-        const abi = ["function incrementContext()"];
-        const contract = new ethers.Contract(counterAddress, abi, signer as any);
-        const { data } = await contract.populateTransaction.incrementContext();
-        const relayRequest = {
-            chainId: chain?.id,
-            target: counterAddress,
-            data: data as BytesLike,
-            user: address,
-        };
-        const _provider = new ethers.providers.Web3Provider(window.ethereum as any);
-        const relayResponse = await GelatoRelaySDK.relayWithSponsoredUserAuthCall(
-            relayRequest as any,
-            _provider,
-            'KORU_DAO_KEY',
-        );
-        console.log(relayResponse);
     };
 
     const getRequestData = async () => {
@@ -118,65 +95,10 @@ export default function SendMessageBox() {
 
     const post = async () => {
         try {
-            setIsGettingSignature(true);
-            const ipfs = await uploadIpfs();
-            const lensProfileId = supportedChains[chain?.id as number].lensProfileId;
-            const contentUri = "https://ipfs.infura.io/ipfs/" + ipfs;
-            const contentModule = supportedChains[chain?.id as number].freeCollectModule;
-            const collectModuleInitData = "0x0000000000000000000000000000000000000000000000000000000000000000";
-            const referenceModule = "0x0000000000000000000000000000000000000000";
-            const referenceModuleInitData = "0x";
-
-            const { address: metaboxAddress, abi: metaboxAbi } =
-                GelatoRelaySDK.getMetaBoxAddressAndABI(chain?.id as number);
-            const contract = koruContract.connect(supportedChains[chain?.id as number].nft, signer as Signer);
-            const metaBox = new ethers.Contract(metaboxAddress, metaboxAbi, provider);
-            const nonce = Number(await metaBox.nonce(address));
-
-            const koruDaoPostData = contract.interface.encodeFunctionData("post", [
-                [
-                    lensProfileId,
-                    contentUri,
-                    contentModule,
-                    collectModuleInitData,
-                    referenceModule,
-                    referenceModuleInitData,
-                ],
-            ]);
-
-            const metaTxRequest = GelatoRelaySDK.metaTxRequest(
-                chain?.id as number,
-                supportedChains[chain?.id as number].koru,
-                koruDaoPostData,
-                "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-                2,
-                ethers.utils.parseEther(supportedChains[chain?.id as number].maxFee).toString(),
-                '20000000',
-                address as string,
-                nonce,
-                supportedChains[chain?.id as number].sponsor,
-            );
-
-            // Get transaction data
-            const metaTxRequestData = GelatoRelaySDK.getMetaTxRequestWalletPayloadToSign(metaTxRequest);
-            const userSignature = await signTypedDataAsync(metaTxRequestData);
-
-            const resp = await fetch('https://relay-sponsor-backend.herokuapp.com/sponsor/sign', {
-                method: "POST",
-                headers: {
-                    cache: "no-cache",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    userSignature,
-                    metaTxRequest,
-                }),
-            });
-
-            if (!resp.ok) {
+            const { taskId } = await makeLensPost();
+            if (!taskId) {
                 throw 'Failed to post message';
             } else {
-                // addPostToPublications(); // has to be improved for new messages do not overwrite
                 setUserMessage('');
                 setIsGettingSignature(false);
                 setIsPosted(true);
@@ -191,10 +113,6 @@ export default function SendMessageBox() {
 
     return (
         <div>
-            <div className="flex gap-3">
-                <span className="border p-3 bg-koru-gray text-white" onClick={() => testIncrementContext()}>Test Increment Context</span>
-                <span className="border p-3 bg-koru-gray text-white" onClick={() => testLensPost()}>Test Lens post</span>
-            </div>
             <div className="koru-box mt-6 lg:mt-10 p-10 min-h-[200px]">
                 <div className="flex gap-4">
                     <UiIcon icon="logo-pic" classes="w-12 h-12" />
