a438698a154f4172d4afc62b01eb8e2f2a1a5320
Renato Cardoso <re2005@gmail.com>
2023-02-13 10:00:43 -0300

    chore: add powered by gelato logo

--- src/components/globals/Header.tsx
+++ src/components/globals/Header.tsx
@@ -1,4 +1,5 @@
 import IconLogo from '../../assets/icons/icon-logo.svg';
+import PoweredGelato from '../../assets/icons/icon-gelato-header.svg';
 import Networks from './Networks';
 import { Account } from './Account';
 import { useAccount } from 'wagmi';
@@ -10,7 +11,14 @@ export default function Header() {
     const { nftId } = useContext(AppContext);
     return (
         <header className="flex flex-col md:flex-row justify-between items-center">
-            <img src={IconLogo} alt="Koru DAO" className="w-40" />
+            <div className="flex gap-5 items-center">
+                <img src={IconLogo} alt="Koru DAO" className="w-40" />
+                <img
+                    src={PoweredGelato}
+                    className="w-36 shrink-0"
+                    alt="Powered by Gelato"
+                />
+            </div>
             <div className="mt-6 md:mt-0 items-center gap-2 md:gap-4 flex">
                 {isConnected &&
                   <>
